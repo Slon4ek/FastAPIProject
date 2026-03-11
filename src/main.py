32dbe33d58731ba093ebf71ffd19936dfc4fac5f
slon4ek <me@slon4ek.ru>
2026-03-11 15:01:02 +0300

    Добавлен роутер для работы с номерами

--- src/main.py
+++ src/main.py
@@ -7,11 +7,13 @@ from fastapi.openapi.docs import (
 )
 from src.api.hotels import router as hotels_router
 from src.api.auth import router as authorization_router
+from src.api.rooms import router as rooms_router
 
 app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(authorization_router)
 
 app.include_router(hotels_router)
+app.include_router(rooms_router)
 
 
 @app.get("/docs", include_in_schema=False)

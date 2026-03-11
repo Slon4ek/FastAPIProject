76d267141f4b422a360ce5b0990f47b4b3f3d833
slon4ek <me@slon4ek.ru>
2026-03-11 15:04:49 +0300

    Описан класс репозиторий для работы с пользователями, добавлен метод на получение пользователя из бд с хешированным паролем

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,8 +1,9 @@
-
+from pydantic.v1 import EmailStr
+from sqlalchemy import select
 
 from src.models.users import UsersOrm
 from src.repositories.base import BaseRepository
-from src.schemas.users import User
+from src.schemas.users import User, UserWithHashedPassword
 
 
 class UserRepository(BaseRepository):
@@ -10,3 +11,9 @@ class UserRepository(BaseRepository):
     name = "User"
     table_name = "users"
     schema = User
+
+    async def get_user_with_hashed_password(self, email: EmailStr) -> UserWithHashedPassword:
+        qwery = select(self.model).filter_by(email=email)
+        result = await self.session.execute(qwery)
+        model = result.scalars().one()
+        return UserWithHashedPassword.model_validate(model)

14aed2c399a4becfabdd78b6f9ac2b7b8950555b
slon4ek <me@slon4ek.ru>
2026-03-11 20:58:20 +0300

    Рефакторинг

--- src/api/auth.py
+++ src/api/auth.py
@@ -13,7 +13,12 @@ router = APIRouter(
 )
 
 
-@router.post("/login")
+@router.post(
+    "/login",
+    summary="Аутентификация пользователя",
+    description="Аутентификация пользователя по логину(email) и паролю, "
+                "генерирует токен доступа и устанавливает его в куки"
+)
 async def login_user(
         data: UserLogin,
         response: Response
@@ -21,12 +26,9 @@ async def login_user(
     async with async_session_maker() as session:
         user = await UserRepository(session).get_user_with_hashed_password(data.email)
         
-        if not user:
-            raise HTTPException(status_code=401, detail="Такой email не зарегистрирован")
-        
-        if not AuthService().verify_password(data.password, user.hashed_password):
-            raise HTTPException(status_code=401, detail="Неверный пароль")
-        
+        if not user or not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Неверный логин или пароль")
+
         access_token = AuthService().create_access_token({
             "user_id": user.id
         })
@@ -34,7 +36,11 @@ async def login_user(
         return {"access_token": access_token}
 
 
-@router.post("/register")
+@router.post(
+    "/register",
+    summary="Регистрация пользователя",
+    description="Регистрация нового пользователя. first_name, last_name - не обязательные поля"
+)
 async def register_user(
         user: UserRequestAdd = Body(
             openapi_examples={
@@ -80,7 +86,11 @@ async def register_user(
     return {"message": "User registered successfully", "user": auth_user}
 
 
-@router.get("/me")
+@router.get(
+    "/me",
+    summary="Получить информацию о текущем пользователе",
+    description="Получение информации о текущем пользователе по токену доступа"
+)
 async def get_me(
         user_id: UserIdDep
 ):
@@ -89,7 +99,11 @@ async def get_me(
     return {"user": user}
 
 
-@router.post("/logout")
+@router.post(
+    "/logout",
+    summary="Выйти из системы",
+    description="Удаление токена доступа из куки"
+)
 async def logout_user(
         response: Response
 ):
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -12,8 +12,10 @@ class UserRepository(BaseRepository):
     table_name = "users"
     schema = User
 
-    async def get_user_with_hashed_password(self, email: EmailStr) -> UserWithHashedPassword:
+    async def get_user_with_hashed_password(self, email: EmailStr) -> UserWithHashedPassword | None:
         qwery = select(self.model).filter_by(email=email)
         result = await self.session.execute(qwery)
-        model = result.scalars().one()
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
         return UserWithHashedPassword.model_validate(model)

7d8019ac2d89d04c9da12db864eeb8db986a4dab
slon4ek <me@slon4ek.ru>
2026-03-11 15:03:06 +0300

    Переписан метод для получения всех номеров конкретного отеля

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,5 +1,18 @@
+from sqlalchemy import select
+
 from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
+from src.schemas.rooms import Room
+
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
+    name = "Room"
+    table_name = "rooms"
+    schema = Room
+
+    async def get_all(self, hotel_id: int):
+        query = select(self.model).filter_by(hotel_id=hotel_id)
+        result = await self.session.execute(query)
+        return [room for room in result.scalars().all()]
+

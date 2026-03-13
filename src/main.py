c8b92806aec5f5c15682a63fc371970bced1ad33
slon4ek <me@slon4ek.ru>
2026-03-13 13:01:25 +0300

    Добавлен роутер для бронирований

--- src/main.py
+++ src/main.py
@@ -8,12 +8,14 @@ from fastapi.openapi.docs import (
 from src.api.hotels import router as hotels_router
 from src.api.auth import router as authorization_router
 from src.api.rooms import router as rooms_router
+from src.api.bookings import router as bookings_router
 
 app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(authorization_router)
 
 app.include_router(hotels_router)
 app.include_router(rooms_router)
+app.include_router(bookings_router)
 
 
 @app.get("/docs", include_in_schema=False)
